9b2147c7e3d916715d079356f0ac39dfe1ddbd2f
unknown <nyandiakamau24@gmail.com>
2022-10-06 11:17:59 +0300

    this is my commit

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# create a manifest that kills a process named killmenow.
+exec { 'killmenow':
+  path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'pkill -f killmenow',
+  #provider => 'shell',
+}
